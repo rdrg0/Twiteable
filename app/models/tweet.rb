7cac0a7477cccba53f79080750ed3eed6b55795a
natlines1492 <natlines14@gmail.com>
2021-08-25 20:40:09 +0000

    Add validations and associations to Like model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,13 +1,11 @@
 class Tweet < ApplicationRecord
   belongs_to :user
-  belongs_to :replied_to, class_name: "Tweet", optional: true, counter_cache: true
+  belongs_to :replied_to, class_name: 'Tweet', optional: true, counter_cache: true
 
-  has_many :replies, class_name: "Tweet",
-                        foreign_key: "replied_to_id",
-                        dependent: :nullify,
-                        inverse_of: "replied_to"
+  has_many :replies, class_name: 'Tweet',
+                     foreign_key: 'replied_to_id',
+                     dependent: :nullify,
+                     inverse_of: 'replied_to'
 
   validates :body, presence: true, length: { maximum: 140 }
-  
-
 end

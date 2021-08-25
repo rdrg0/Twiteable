55016bd3d0164b70637765695457c4a130eb2d10
natlines1492 <natlines14@gmail.com>
2021-08-25 17:28:36 +0000

    Add user validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: true
+
+  enum role: { member: 0, admin: 1 }
+
+  has_many :tweets, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  
+
+
 end

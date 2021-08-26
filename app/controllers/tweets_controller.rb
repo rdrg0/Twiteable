319d733741dd92c2708aa16a0db646b719bf6e3d
natlines1492 <natlines14@gmail.com>
2021-08-26 04:59:39 +0000

    Fix conflicts

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,20 @@
 class TweetsController < ApplicationController
-<<<<<<< HEAD
   def index
     @tweets = Tweet.all
-=======
+  end
+
   def show
     @tweet = Tweet.find(params[:id])
     @new_tweet = Tweet.new
->>>>>>> main
   end
+end
+
+# def create
+#   user = User.first  # current_user
+# end
+
+private
+
+def tweet_params
+  params.require(:tweet).permit(:body)
 end
